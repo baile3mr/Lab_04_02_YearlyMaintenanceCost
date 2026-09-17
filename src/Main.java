9664ce13ab36f0a502747fc21f985e4bbf97091c
Meaghan Bailey <baile3mr@mail.uc.edu>
2026-09-17 14:50:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main()
+{
+    double spring = 340.78;
+    double fall = 245;
+    double winter = 0;
+    double summer = 0;
+    double totalCost = 0;
+
+    totalCost = spring + summer + fall + winter;
+    IO.println("The total cost is " + totalCost);
+}
